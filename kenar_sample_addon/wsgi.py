5c21cba4d2db39c00dd367ac890ca09edb262144
AmirSalar Safaei <amirsalar.safaei@divar.ir>
2023-10-01 15:33:40 +0330

    init: added gitignore and started django project

---
+++ kenar_sample_addon/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for kenar_sample_addon project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kenar_sample_addon.settings')
+
+application = get_wsgi_application()
